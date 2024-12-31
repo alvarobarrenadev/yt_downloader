d99e621a49765a4c79b63a86c8907c972dd58f41
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2024-12-31 15:17:14 +0100

    Add concurrent download functionality for multiple YouTube videos

--- app2.py
+++ app2.py
@@ -1,6 +1,7 @@
 import yt_dlp
+from concurrent.futures import ThreadPoolExecutor
 
-def descargar__multiples_videos_yt(url, formato='video'):
+def descargar_video_yt(url, formato='video'):
     opciones = {
         'format': 'bestvideo+bestaudio/best',
         'outtmpl': '%(title)s.%(ext)s',
@@ -22,10 +23,21 @@ def descargar__multiples_videos_yt(url, formato='video'):
     except Exception as e:
         print(f"Hubo un error con {url}: {e}")
 
+def descargar_multiples_videos(urls, formato='video', num_hilos=4):
+    print(f"Iniciando descargas en formato: {formato}...")
+    with ThreadPoolExecutor(max_workers=num_hilos) as executor:
+        futures = [executor.submit(descargar_video_yt, url, formato) for url in urls]
+        for future in futures:
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Error durante la descarga: {e}")
+
 if __name__ == "__main__":
     urls = [
-        "",
+        ""
     ]
+
+
     formato = input("¿En qué formato deseas descargar (video/audio)?: ").lower()
-    for url in urls:
-        descargar__multiples_videos_yt(url, formato)
+    descargar_multiples_videos(urls, formato, num_hilos=10)
